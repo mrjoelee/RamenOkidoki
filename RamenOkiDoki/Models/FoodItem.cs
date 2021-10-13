6419e73c6aaf2536d5d21f646bac0ca5129fd7bf
BillyMartin1964 <billy@sitesculptors.com>
2021-10-13 06:05:46 -0400

    Added Korean name

--- RamenOkiDoki/Models/FoodItem.cs
+++ RamenOkiDoki/Models/FoodItem.cs
@@ -4,6 +4,7 @@
     {
             public string id { get; set; }
             public string dishName { get; set; }
+            public string koreanName { get; set; }
             public string description { get; set; }
             public string price { get; set; }
             public string categoryName { get; set; }
