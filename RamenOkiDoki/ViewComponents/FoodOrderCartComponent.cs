435f00eb096034293c3e4569b40ee2d887449813
BillyMartin1964 <billy@sitesculptors.com>
2021-10-15 07:04:35 -0400

    Cart shows up in takeout

--- RamenOkiDoki/ViewComponents/FoodOrderCartComponent.cs
+++ RamenOkiDoki/ViewComponents/FoodOrderCartComponent.cs
@@ -4,20 +4,46 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using RamenOkiDoki.Models;
+using RamenOkiDoki.Services;
+using RamenOkiDoki.ViewModels;
 
 namespace RamenOkiDoki.ViewComponents
 {
     public class FoodOrderCartViewComponent : ViewComponent
     {
-        public FoodOrderCartViewComponent()
+        private MenuEndpointService _menuEndpointService { get; set; }
+        public FoodOrderCartViewComponent(MenuEndpointService menuEndpointService)
         {
-
+            _menuEndpointService = menuEndpointService;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-           // ViewBag.CurrentCity = await GetCity(cityName);
-            return View("FoodOrderCart");
+            Globals.FoodItems = await _menuEndpointService.GetFoodItemsFromCloud();
+
+            if (Globals.FoodItems == null)
+            {
+                return null;
+            }
+
+
+            FoodItemsViewModel foodItemsViewModel = new FoodItemsViewModel();
+            List<OrderItem> tempOrderList = new List<OrderItem>();
+
+            foreach (var item in Globals.FoodItems)
+            {
+                tempOrderList.Add(
+                    new OrderItem() { id = item.id, dishName = item.dishName, koreanName = item.koreanName, price = item.price, quantity = 1 });
+            }
+
+            foodItemsViewModel.OrderedItems = tempOrderList;
+
+            if (foodItemsViewModel.OrderedItems == null)
+            {
+
+            }
+            return View("FoodOrderCart", foodItemsViewModel);
         }
     }
 }
