93ddd0e00bba335c0adbb1b9e1145c5e1a8c6344
BillyMartin1964 <billy@sitesculptors.com>
2021-10-20 06:55:07 -0400

    Hours and Location Work

--- Data/ViewModels/DashboardViewModel.cs
+++ Data/ViewModels/DashboardViewModel.cs
@@ -25,6 +25,24 @@ namespace Data.ViewModels
                 PhoneNumber = "2815758077",
                 Email = ""
             };
+
+            HoursOfOperation = new HoursOfOperation()
+            {
+                SundayOpen = "10",
+                SundayClose = "10",
+                MondayOpen = "10",
+                MondayClose = "10",
+                TuesdayOpen = "10",
+                TuesdayClose = "10",
+                WednesdayOpen = "10",
+                WednesdayClose = "10",
+                ThursdayOpen = "10",
+                ThursdayClose = "10",
+                FridayOpen = "10",
+                FridayClose = "10",
+                SaturdayOpen = "10",
+                SaturdayClose = "10"
+            };
         }
     }
 
