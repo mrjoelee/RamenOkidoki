172430c10c2b16151ae1cb737c7d0582b09ac54d
BillyMartin1964 <billy@sitesculptors.com>
2021-10-21 13:47:35 -0400

    PhotoViewModel

---
+++ RamenOkiDoki/ViewComponents/PhotoCarouselViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RamenOkiDoki.ViewComponents
+{
+    public class PhotoCarouselViewComponent : ViewComponent
+    {
+        public PhotoCarouselViewComponent()
+        {
+
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        { 
+            PhotosViewModel photosViewModel = new PhotosViewModel();
+
+
+            return View("PhotoCarousel", photosViewModel);
+        }
+    }
+}
