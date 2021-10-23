d7b246f18baedecd531e30fbd786697d89da4cd9
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-10-22 23:37:38 -0400

    commented on start up and program to understand MVC

--- RamenOkiDoki/Program.cs
+++ RamenOkiDoki/Program.cs
@@ -12,11 +12,13 @@ namespace RamenOkiDoki
 {
     public class Program
     {
+        //main function - starts as a main console application but calling the method IHostbuilder
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        //create web application
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
