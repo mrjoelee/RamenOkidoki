7ada73906f8c1c968e1a9e487b6d172feae229bd
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-10-26 09:19:26 -0400

    added reviews

--- Data/ViewModels/DashboardViewModel.cs
+++ Data/ViewModels/DashboardViewModel.cs
@@ -53,11 +53,22 @@ namespace Data.ViewModels
 
             Reviews = new List<Review>()
             {
+                new Review( "Erica P.", "The ramen and garlic soy wings were delish. The customer service was also great!", 5),
+
+                new Review( "Nhan V.", "I LOVE THIS PLACE!! I go here at least once a week ahaha! The owner is super nice and it's quite impressive how he manage this whole place by himself sometimes.", 5),
+
+                new Review( "AD Avenue G.", "Good taste. Staff is friendly clean place. I took the tonkotsu ramen with an extra egg. Delicious.", 5),
+
+                new Review( "Lv126 M.", "Great ramen for a good price. I recommend coming here.", 5),
+
                 new Review("Sharon Ree", "Very true to the Japanese and Korean pub style interior. One thing… throughout the meal, the metal chairs were very loud, screeching against the floor, every time someone got up or sat back down…We had spicy chicken wings, takoyaki, and spicy miso and the house okidoki ramen… they were all well cooked, perfectly seasoned, and Bathroom is odd. Hallway and bathroom itself were Houston hot. Meaning it’s not air conditioned. Maybe an exterior addition… spicy miso wasn’t that spicy. It was more medium korean spicy :) but I got more spice on the side.The service was good, nothing special, , parking aplenty outside, the bathrooms were odd and the hallway and the bathroom itself aren’t air conditioned so you are met with a wall of hot, humid air. It seems like a last minute add on.. but having access to bathrooms during covid is good.. we will def return and try other things, because the food is solid and my kids loved it.", 4 ),
 
                 new Review("Serena S.", "Nice little gem in China town! We came here yesterday at about 6 pm; we were the only ones in the restaurant! Really enjoyed the fries; probably as good and comparable to McDonald's but the toppings were extra tasty. Both of our portions were amazing for the ramen!! I had so much to take home which I feel is a little rare for ramen places. I think the visuals were really nice. I took off one star because while the ramen was okay I think my boyfriends broth was just a tad on the watery side. Overall will be coming back the next time I'm craving some ramen!", 4 ),
 
-                new Review( "Lan N.", "This is definitely my go-to ramen place. On Thursdays from 5PM until close, they have a $6.99 special on ANY ramen. This is definitely a steal! The ramen itself tastes good - broth is flavorful and noodles have a good texture. Only downside is that the wait is a little long for your ramen to come out, but other than that, I definitely recommend!", 4)
+                new Review( "Lan N.", "This is definitely my go-to ramen place. On Thursdays from 5PM until close, they have a $6.99 special on ANY ramen. This is definitely a steal! The ramen itself tastes good - broth is flavorful and noodles have a good texture. Only downside is that the wait is a little long for your ramen to come out, but other than that, I definitely recommend!", 4),
+
+                new Review( "Stephanie D.", "Good ramen. Been coming here for about 2 years. I have notice that before the pork meat use to be thick now it is very thin. But it is still very good flavor.", 4)
+
             };
         }
     }
