80040bd5397191550b9944a931eb7bd12426275f
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-10-26 22:17:30 -0400

    created start clickable rating - however hovering doens't work perfectly.

---
+++ RamenOkiDoki/wwwroot/js/rating-star.js
@@ -0,0 +1,30 @@
+﻿////function CRateOut(rating) {
+////    for (var i = 1; i <= rating; i++) {
+////        $(".class" + i).attr('class', 'far fa-star');
+////    }
+////}
+
+
+////function CRateover(rating) {
+////    for (var i = 1; i <= rating; i++) {
+////        $(".class" + i).attr('class', 'fas fa-star');
+////    }
+////}
+
+////function CRateClick(rating) {
+////    $("lblRating").val(rating);
+////    for (var i = 1; i <= rating; i++) {
+////        $(".class" + i).attr('class', 'fas fa-star');
+////    }
+
+////    for (var i = rating + 1 ; i <= 5; i++) {
+////        $(".class" + i).attr('class', 'far fa-star');
+////    }
+////}
+
+////function CRateSelected() {
+////    var rating = $("lblRating").val(rating);
+////    for (var i = 1; i <= rating; i++) {
+////        $(".class" + i).attr('class', 'fas fa-star');
+////    }
+////}
