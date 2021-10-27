908bc1b6d2d7c6e7d022bb520e654b9eb2fe2c7b
BillyMartin1964 <billy@sitesculptors.com>
2021-10-27 07:39:35 -0400

    Moved Javascipt to separate file

--- RamenOkiDoki/wwwroot/js/rating-star.js
+++ RamenOkiDoki/wwwroot/js/rating-star.js
@@ -1,56 +1,33 @@
-﻿////$(function() {
-////    /* function CRateClick(rating) {*/
-////    $('#lblRating').on('click',
-////        function() {
-////        {
-////            for (var i = 1; i <= rating; i++) {
-////                $("#span" + i).attr('class', 'fas fa-star');
-////            }
+﻿function CRateClick(rating) {
+    $("#lblRating").val(rating);
+    for (var i = 1; i <= rating; i++) {
+        $("#span" + i).attr('class', 'fas fa-star');
+    }
 
-////            for (var i = rating + 1; i <= 5; i++) {
-////                $("#span" + i).attr('class', 'far fa-star');
-////            }
-////        });
+    for (var i = rating + 1; i <= 5; i++) {
+        $("#span" + i).attr('class', 'far fa-star');
+    }
 
-////    /* function CRateSelected() {*/
-////    $('#lblRating').on('mouseover',
-////        function() {
-////            var rating = $("#lblRating").val();
-////            for (var i = 1; i <= rating; i++) {
-////                $("#span" + i).attr('class', 'far fa-star');
-////            });
-////    /*  }*/
-
-////});
+}
 
 
-////function CRateOut(rating) {
-////    for (var i = 1; i <= rating; i++) {
-////        $(".class" + i).attr('class', 'far fa-star');
-////    }
-////}
+//function CRateOut(rating) {
+//    for (var i = 1; i <= rating; i++) {
+//        $("#span" + i).attr('class', 'far fa-star');
+//    }
+//}
 
 
-////function CRateover(rating) {
-////    for (var i = 1; i <= rating; i++) {
-////        $(".class" + i).attr('class', 'fas fa-star');
-////    }
-////}
+//function CRateOver(rating) {
+//    for (var i = 1; i <= rating; i++) {
+//        $("#span" + i).attr('class', 'fas fa-star');
+//    }
+//}
 
-////function CRateClick(rating) {
-////    $("lblRating").val(rating);
-////    for (var i = 1; i <= rating; i++) {
-////        $(".class" + i).attr('class', 'fas fa-star');
-////    }
 
-////    for (var i = rating + 1 ; i <= 5; i++) {
-////        $(".class" + i).attr('class', 'far fa-star');
-////    }
-////}
-
-////function CRateSelected() {
-////    var rating = $("lblRating").val(rating);
-////    for (var i = 1; i <= rating; i++) {
-////        $(".class" + i).attr('class', 'fas fa-star');
-////    }
-////}
+//function CRateSelected() {
+//    var rating = $("#lblRating").val();
+//    for (var i = 1; i <= rating; i++) {
+//        $("#span" + i).attr('class', 'far fa-star');
+//    }
+// }
