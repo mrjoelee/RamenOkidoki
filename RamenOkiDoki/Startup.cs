73823fb32a80961dc669c4eb561529d94a66b22c
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-11-13 15:23:06 -0500

    moved Counter razor component inside page folder

--- RamenOkiDoki/Startup.cs
+++ RamenOkiDoki/Startup.cs
@@ -103,7 +103,7 @@ namespace RamenOkiDoki
 
                 endpoints.MapRazorPages();
 
-                endpoints.MapBlazorHub("/Blazor/_blazor");
+                endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("~/Blazor/{*clientroutes:nonfile}", "/Blazor/_Host");
 
                 //endpoints.MapBlazorHub();
